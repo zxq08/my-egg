727b4b80fea50eb2bd108f3ec3bc4ad522400527
zxq <461890276@qq.com>
2022-01-16 10:26:02 +0800

    addNewController

--- app/controller/home.js
+++ app/controller/home.js
@@ -7,6 +7,10 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
+  async inde2x() {
+    const { ctx } = this;
+    ctx.body = 'hi, egg';
+  }
 }
 
 module.exports = HomeController;
