ea7a4936c6dae9dd007ca35d738b20d3c12c1415
zxq <461890276@qq.com>
2022-01-16 17:33:26 +0800

    addExtend

--- app/controller/home.js
+++ app/controller/home.js
@@ -11,7 +11,22 @@ class HomeController extends Controller {
   async inde2x() {
     const { ctx,app } = this;
     console.log(ctx.session.counter)
+    console.log(ctx.params())
     ctx.body = `hi, hello eggjs!\r\nnow: ${app.currentTime()}`;
+  }
+  async indexWithToken() {
+    const { ctx } = this;
+    const token = ctx.request.token
+    ctx.body = {
+      status: 200,
+      body: token
+    };
+  }
+  async newResponse() {
+    const {ctx} = this;
+    ctx.response.token = 'xq-z'
+    const testBase64 = ctx.helper.base64Encode('lszxq.cn')
+    ctx.body = testBase64
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -7,5 +7,7 @@ module.exports = app => {
   const counter = app.middleware.counter()
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  router.post('/token', controller.home.indexWithToken);
+  router.get('/setToken', controller.home.newResponse);
   router.get('/myegg', counter, controller.home.inde2x);
 };
